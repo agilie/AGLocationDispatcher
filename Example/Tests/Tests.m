8c140b0885d44b866efe7dc1ff44698b503f2d67
kalamaznik <vladimir.zgonik@ideas-world.com>
2015-02-13 18:44:39 +0200

    test deleted

--- Example/Tests/Tests.m
+++
@@ -1,9 +0,0 @@
-//
-//  AGLocationDispatcherTests.m
-//  AGLocationDispatcherTests
-//
-//  Created by Vladimir Zgonik on 02/12/2015.
-//  Copyright (c) 2014 Agilie. All rights reserved.
-//
-
-//${TEST_EXAMPLE}
