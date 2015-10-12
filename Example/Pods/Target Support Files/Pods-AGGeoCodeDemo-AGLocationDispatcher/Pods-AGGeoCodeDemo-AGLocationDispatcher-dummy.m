9ba820933130cb8ffdec282fb8b00910ecf85481
alexankudinov <alexander.ankudinov@agilie.com>
2015-10-12 16:08:29 +0300

    - fix pod target names #1

--- Example/Pods/Target Support Files/Pods-AGGeoCodeDemo-AGLocationDispatcher/Pods-AGGeoCodeDemo-AGLocationDispatcher-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AGGeoCodeDemo_AGLocationDispatcher : NSObject
-@end
-@implementation PodsDummy_Pods_AGGeoCodeDemo_AGLocationDispatcher
-@end
--- Example/Pods/Target Support Files/Pods-AGGeoRegionTrackingDemo-AFNetworking/Pods-AGGeoRegionTrackingDemo-AFNetworking-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AGGeoRegionTrackingDemo_AFNetworking : NSObject
-@end
-@implementation PodsDummy_Pods_AGGeoRegionTrackingDemo_AFNetworking
-@end
